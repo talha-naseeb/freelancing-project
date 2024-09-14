0c8931466a82865699d9bc0f0f7b02f2680ac3d5
talha-naseeb <talhanaseeb27@gmail.com>
2024-09-15 02:48:30 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Ensure that all 404 requests are served with index.html to handle client-side routing
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
+  // This makes sure that Vite serves index.html for all unknown routes
+  server: {
+    historyApiFallback: true,
+  },
+});
